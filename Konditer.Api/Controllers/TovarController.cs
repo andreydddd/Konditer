7eb5fef0d23d85ad99c72d770440cd254bd25a66
andreydddd <104488789+andreydddd@users.noreply.github.com>
2023-12-29 21:32:32 +0300

    Add files via upload

---
+++ Konditer.Api/Controllers/TovarController.cs
@@ -0,0 +1,69 @@
+﻿using Konditer.Context.Contracts.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Konditer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TovarController : ControllerBase
+    {
+        private readonly IContext context1;
+        public TovarController(IContext context1)
+        {
+            this.context1 = context1;
+        }
+        [HttpGet] //localhost:111224/group
+        public IActionResult GetAllTovar()
+        {
+            var tovarlist = context1.Tovars.ToList();
+            return Ok(tovarlist);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var tovarlist = context1.Tovars.FirstOrDefault(x => x.Id == id);
+            return Ok(tovarlist);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var group3 = context1.Tovars.FirstOrDefault(x => x.Id == id);
+            if (group3 != null)
+            {
+                context1.Tovars.Remove(group3);
+            }
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Create(Tovar model)
+        {
+            var item3 = new Tovar
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Opis = model.Opis,
+                Value = model.Value,
+                CreatedAT = DateTime.Now,
+                CreatedBy = "Я",
+                UpdatedAt = DateTime.Now,
+                UpdatedBy = "Я богатырь!"
+            };
+            context1.Tovars.Add(item3);
+            return Ok(item3);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(Guid Id, Tovar model)
+        {
+            var group3 = context1.Tovars.FirstOrDefault(x => x.Id == Id);
+            if (group3 != null)
+            {
+                return NotFound();
+            }
+            group3.Name = model.Name;
+            group3.Opis = model.Opis;
+            group3.Value = model.Value;
+            group3.UpdatedAt = DateTime.Now;
+            return Ok(group3);
+        }
+    }
+}
---
+++ Konditer.Context/SVContext.cs
@@ -0,0 +1,102 @@
+﻿using Konditer.Context.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konditer.Context
+{
+    public class SVContext : IContext
+    {
+        private ICollection<Nakladnaya> nakladnayas;
+        private ICollection<Pokupatel> pokupatels;
+        private ICollection<Postavshik> postavshiks;
+        private ICollection<Tovar> tovars;
+
+        public SVContext()
+        {
+            nakladnayas = new HashSet<Nakladnaya>();
+            pokupatels = new HashSet<Pokupatel>();
+            postavshiks = new HashSet<Postavshik>();
+            tovars = new HashSet<Tovar>();
+            Seed();
+        }
+        public ICollection<Nakladnaya> Nakladnayas => nakladnayas;
+
+        public ICollection<Pokupatel> Pokupatels => pokupatels;
+
+        public ICollection<Postavshik> Postavshiks => postavshiks;
+
+        public ICollection<Tovar> Tovars => tovars;
+
+        private void Seed()
+        {
+            Nakladnayas.Add(new Nakladnaya
+            {
+                Id = Guid.NewGuid(),
+                Name = "ОАО Цикада",
+                Description = "Накладная № 1254353554, ИНН 13141431, КПП 14321312, Адрес покупателя: Рыбинск"
+            });
+            Nakladnayas.Add(new Nakladnaya
+            {
+                Id = Guid.NewGuid(),
+                Name = "ООО Цербер",
+                Description = "Накладная № 3453454234, ИНН 1234543522212, КПП 135345552, Адрес покупателя: Сингапур"
+            });
+            Pokupatels.Add(new Pokupatel
+            {
+                Id = Guid.NewGuid(),
+                Name = "Павленко Илья Евгеньевич",
+                Number = 799102950,
+            });
+            Pokupatels.Add(new Pokupatel
+            {
+                Id = Guid.NewGuid(),
+                Name = "Рукенглаз Андрей Алексеевич",
+                Number = 793221122,
+            });
+            Postavshiks.Add(new Postavshik
+            {
+                Id = Guid.NewGuid(),
+                Email = "maga@mail.ru",
+                Name = "Магамед Мага Магамедович",
+                Adres = "Австралия, Жинь-Шинь 21",
+                Index = 222233,
+                Inn = 123132131,
+                RS = 464563362
+            });
+            Postavshiks.Add(new Postavshik
+            {
+                Id = Guid.NewGuid(),
+                Email = "shtorm@mail.ru",
+                Name = "Джонсон Джексон Шторм",
+                Adres = "Китай, Хуй-Вынь",
+                Index = 543523,
+                Inn = 23434341,
+                RS = 213125487
+            });
+            Tovars.Add(new Tovar
+            {
+                Id = Guid.NewGuid(),
+                Name = "Яблочное безумие",
+                Opis = "Хлебное изделие с яблочным повидлом",
+                Value = 35
+            });
+            Tovars.Add(new Tovar
+            {
+                Id = Guid.NewGuid(),
+                Name = "Торт Черепашка",
+                Opis = "Шоколадная нуга, посыпка, коржик",
+                Value = 2
+            });
+            Tovars.Add(new Tovar
+            {
+                Id = Guid.NewGuid(),
+                Name = "Арахисовое пироженное",
+                Opis = "Арахис, сливки, хлеб, арахисовая паста",
+                Value = 15
+            });
+        }
+    }
+}
